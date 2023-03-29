a361a751d08f1ac2385c14c97a0c862f4e39d575
Karl Lausmaa <karl.lausmaa@gmail.com>
2023-03-29 20:23:26 +0300

    for loops

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿int userPin;
+int triesUsed = 0;
+
+while(triesUsed != 3)
+{
+    
+    Console.WriteLine("Enter your pin:");
+    userPin = Int32.Parse(Console.ReadLine());
+}if(userPin == 1234)
+{
+    Console.WriteLine("Welcome");
+    triesUsed = 3;
+}else
+{
+    Console.WriteLine($"Wrong PIN. Try again {triesUsed}");
+    triesUsed++;
+}
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,6 @@
+﻿
+
+for(int i =0; i < 5; i++)
+{
+    Console.WriteLine("Hi!");
+}
